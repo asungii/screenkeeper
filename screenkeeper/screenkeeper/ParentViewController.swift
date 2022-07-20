8b0ace027d07a19c1458f3587474dafa285f007c
asungii <kim.laerdon@gmail.com>
2022-07-20 03:22:40 -0500

    test? im confused

--- screenkeeper/screenkeeper/ParentViewController.swift
+++ screenkeeper/screenkeeper/ParentViewController.swift
@@ -1,14 +1,19 @@
 import UIKit
 import FirebaseDatabase
 import Foundation
+import UICircularProgressRing
 
 class ParentViewController: UIViewController {
 
     let defaults = UserDefaults.standard
     let database = Database.database().reference()
+    let shapeLayer = CAShapeLayer()
+    let trackLayer = CAShapeLayer()
     
     private var childTime: Int = -1
     private var childUsername: String = "noname"
+    
+    private var timer = Timer()
     
     let username = UserDefaults.standard.string(forKey: "username") ?? "noname"
             
@@ -47,18 +52,42 @@ class ParentViewController: UIViewController {
                 
         // somehow moving this to the viewdidload worked?? for fixing the first click "noname" shit
         database.child("parent_users/\(username)/device0").observeSingleEvent(of: .value, with: { snapshot in
-            /*self.defaults.set(snapshot.value as? String ?? "noname", forKey: "childUsername")
-            print(self.defaults.string(forKey: "childUsername"))
-             */
             
             self.childUsername = snapshot.value as? String ?? "noname"
         })
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2*CGFloat.pi, clockwise: true)
+        
+        trackLayer.path = circularPath.cgPath
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = UIColor.orange.cgColor
+        shapeLayer.lineWidth = 20
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = 0
+        
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 20
+        trackLayer.lineCap = .round
+        trackLayer.fillColor = UIColor.clear.cgColor
         
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBlue
         view.addSubview(timerDisplay)
         view.addSubview(addTimeField)
         view.addSubview(addTimeButton)
+        view.layer.addSublayer(trackLayer)
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    func triggerTimerRingAnimation(_ duration: Int) {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = CFTimeInterval(duration) * 1.25 // i have no idea why this doesn't work right but it just needs to be *1.25 idk
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "basic")
     }
     
     override func viewDidLayoutSubviews() {
@@ -68,58 +97,67 @@ class ParentViewController: UIViewController {
         timerDisplay.frame = CGRect(x: width/2 - 150, y: height/2 - 150, width: 300, height: 300)
         addTimeField.frame = CGRect(x: width/2 - 150, y: timerDisplay.frame.origin.y + timerDisplay.frame.size.height + 10, width: 183, height: 50)
         addTimeButton.frame = CGRect(x: width/2 - 150 + 193, y: timerDisplay.frame.origin.y + timerDisplay.frame.size.height + 10, width: 107, height: 50)
+        
+        trackLayer.bounds = CGRect(x: width/2 - 150, y: height/2 - 150, width: 300, height: 300)
+        trackLayer.frame = CGRect(x: width/2 - 150, y: height/2 - 150, width: 300, height: 300)
+        shapeLayer.bounds = CGRect(x: width/2 - 150, y: height/2 - 150, width: 300, height: 300)
+        shapeLayer.frame = CGRect(x: width/2 - 150, y: height/2 - 150, width: 300, height: 300)
+
     }
 
     override func viewDidAppear(_ animated: Bool) {
         
         database.child("parent_users/\(username)/device0").observeSingleEvent(of: .value, with: { snapshot in
-            /*self.defaults.set(snapshot.value as? String ?? "noname", forKey: "childUsername")
-            print(self.defaults.string(forKey: "childUsername"))
-             */
             
             self.childUsername = snapshot.value as? String ?? "noname"
         
-            var timeEnd = -1.0
+            var endTime = -1.0
             
             self.database.child("child_users/\(self.childUsername)/time_end").observe(.value, with: { [weak self] snapshot in
                 guard let strongSelf = self else {
                     return
                 }
                 
-                timeEnd = snapshot.value as? Double ?? -1.0
+                endTime = snapshot.value as? Double ?? -1.0
                 
+                var timerDuration = 0
+                
+                let endTimeDate = strongSelf.doubleToDate(endTime)
+                let timeInterval = endTimeDate.timeIntervalSinceNow
+                
+                if endTimeDate < Date() {
+                    timerDuration = 0
+                }
             })
             // timer outside here, somehow get timeEnd variable out here
-            
-            let timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
-                self.updateTimer(timeEnd)
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
+                self.updateTimer(endTime)
+                
                 // issue is, timeEnd is static-- it doesn't change even when it changes in firebase.
             })
         })
+        
     }
     
     @objc private func timerAction() {
     }
     
-    func updateTimer(_ timeEnd: Double) {
-        let timeEndDate = doubleToDate(timeEnd)
-        let timeInterval = timeEndDate.timeIntervalSinceNow
+    func updateTimer(_ endTime: Double) {
+        let endTimeDate = doubleToDate(endTime)
+        let timeInterval = endTimeDate.timeIntervalSinceNow
         
-        if timeEndDate < Date() {
+        if endTimeDate < Date() {
             timerDisplay.text = "0"
             return
         }
-        
+        print((timeInterval))
         timerDisplay.text = String(Int(round(timeInterval)))
     }
 
     
     @objc private func addTimeButtonTapped() {
-        
+                
         database.child("parent_users/\(username)/device0").observeSingleEvent(of: .value, with: { snapshot in
-            /*self.defaults.set(snapshot.value as? String ?? "noname", forKey: "childUsername")
-            print(self.defaults.string(forKey: "childUsername"))
-             */
             
             self.childUsername = snapshot.value as? String ?? "noname"
         
@@ -127,27 +165,22 @@ class ParentViewController: UIViewController {
             
             self.database.child("child_users/\(self.childUsername)/time_end").observeSingleEvent(of: .value, with: { snapshot in
                 
-                var oldEndTimer = snapshot.value as? Double ?? -1.0
-                print(snapshot.value)
-                print(Date())
+                var oldEndTime = snapshot.value as? Double ?? -1.0
                 
-                if oldEndTimer < self.dateToDouble(Date()) {
-                    print("if statement true")
-                    oldEndTimer = self.dateToDouble(Date())
+                // handling if time <= 0
+                if oldEndTime < self.dateToDouble(Date()) {
+                    oldEndTime = self.dateToDouble(Date())
                 }
                 
-                print(oldEndTimer)
-                
-                // THIS HAD TO GO INSIDE!!! THINGS OUTSIDE THE CLOSURE RUN TOGETHER, FIRST!!!
-                              
-                let startTimer = oldEndTimer
-                
-                let endTimer = oldEndTimer + (Double(self.addTimeField.text ?? "0") ?? 0.0)
+                // finding end time
+                let endTime = oldEndTime + (Double(self.addTimeField.text ?? "0") ?? 0.0)
                                 
-                self.database.child("child_users/\(self.childUsername)/time_end").setValue(endTimer)
+                // setting end time
+                self.database.child("child_users/\(self.childUsername)/time_end").setValue(endTime)
                 
-                // the new endTimerDate is becoming the amount of time from ten added to the oldTimerDate
-                
+                self.updateTimer(endTime)
+                print("timer : \((endTime - Date().timeIntervalSince1970))")
+                self.triggerTimerRingAnimation(Int(self.timerDisplay.text ?? "0") ?? 0)
             })
         })
     }
